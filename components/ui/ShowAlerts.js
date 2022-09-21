590e670dd6919688c086bd506f3ad4d4808fbad5
JuJu <laushiju@gmail.com>
2022-09-21 06:35:06 -0700

    0921

--- components/ui/ShowAlerts.js
+++ components/ui/ShowAlerts.js
@@ -2,11 +2,15 @@ import styles from "./ShowAlerts.module.css";
 const Alert = ({ onClose }) => {
   return (
     <div className={styles.container}>
-      <h1>Please call us</h1>
-      <h1> @ </h1>
-      <h1>844-743-4344 </h1>
-      <h1>for help</h1>
-      <button onClick={onClose}>OK</button>
+      <div>
+        <h1>Please call us </h1>
+        <h1> @ </h1>
+        <h1> 844-743-4344 </h1>
+        <h1> for help </h1>
+      </div>
+      <div>
+        <button onClick={onClose}>OK</button>
+      </div>
     </div>
   );
 };
