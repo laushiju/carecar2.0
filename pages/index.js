284166acce86a93d43915e557d5f067641233caf
JuJu <laushiju@gmail.com>
2022-09-21 06:21:51 -0700

    0921

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/Header/Header";
 import Announcement from "../components/Header/Anouncement";
 import MemberService from "../components/memberService/MemberService";
 import CPService from "../components/cpService/CPService";
-import ContactForm from "../components/contact/contactForm";
+import ContactForm from "../components/contact/ContactForm";
 import Footer from "../components/Footer/Footer";
 
 export default function Home() {
