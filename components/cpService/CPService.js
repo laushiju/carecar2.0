d885b8fbcf0a8d113458912623d8222316b68156
JuJu <laushiju@gmail.com>
2022-09-22 09:33:52 -0700

    0922

--- components/cpService/CPService.js
+++ components/cpService/CPService.js
@@ -12,48 +12,50 @@ export default function CPService() {
     setShowModal(true);
   };
   return (
-    <div className={styles.container}>
-      <h1>Care Partner Services</h1>
-      <div>
-        <p>
-          <a onClick={clickHandler}>
-            <RdButton>
-              <Image
-                src="/CC-care-partner.png"
-                alt="CarePartner"
-                width={80}
-                height={80}
-              />
-              <h2>Driver Solutions</h2>
-            </RdButton>
-          </a>
-          <Link href="/">
-            <a>
+    <>
+      <div className={styles.container}>
+        <h1>Care Partner Services</h1>
+        <div>
+          <p>
+            <a onClick={clickHandler}>
               <RdButton>
                 <Image
-                  src="/Member+Icon+(6).png"
+                  src="/CC-care-partner.png"
                   alt="CarePartner"
                   width={80}
                   height={80}
                 />
-                <h2>Care Partner</h2>
+                <h2>Driver Solutions</h2>
               </RdButton>
             </a>
-          </Link>
-          <Link href="/">
-            <a>
-              <RdButton>
-                <Image
-                  src="/CC-health-plan.png"
-                  alt="HealthPlanPartner "
-                  width={80}
-                  height={80}
-                />
-                <h2>Health Plan Partner</h2>
-              </RdButton>
-            </a>
-          </Link>
-        </p>
+            <Link href="/">
+              <a onClick={clickHandler}>
+                <RdButton>
+                  <Image
+                    src="/Member+Icon+(6).png"
+                    alt="CarePartner"
+                    width={80}
+                    height={80}
+                  />
+                  <h2>Care Partner</h2>
+                </RdButton>
+              </a>
+            </Link>
+            <Link href="/">
+              <a onClick={clickHandler}>
+                <RdButton>
+                  <Image
+                    src="/CC-health-plan.png"
+                    alt="HealthPlanPartner "
+                    width={80}
+                    height={80}
+                  />
+                  <h2>Health Plan Partner</h2>
+                </RdButton>
+              </a>
+            </Link>
+          </p>
+        </div>
       </div>
       {showModal && (
         <ShowAlerts
@@ -69,6 +71,6 @@ export default function CPService() {
           }}
         />
       )}
-    </div>
+    </>
   );
 }
